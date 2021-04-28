74a75ef8ae441017ea0d795135042d503e1db9ce
ankita15agarwal <ankita.agarwal@intelegencia.com>
2021-04-28 15:06:29 +0530

    assignment

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import json
 
+import json
 a = input("Enter the name of the car : ")
-f = open('details.json', )
-data = json.load(f)
-for i in data['CarDetails']:
-    print(i)
+data = open('details.json', )
+obj = json.load(data)
+for x in obj["Detail"]:
+   if (x['name'] == a):
+      print(x)
+
+
+
